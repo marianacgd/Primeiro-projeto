163aaede3647be7a727cb9ad6e0ceb588319e7a2
Mariana Daruix <mariana.daruix@gmail.com>
2023-02-16 19:32:37 -0300

    Atualiza Primeiro-projeto

---
+++ src/components/galeria/galeria.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'galeria',
+  templateUrl: './galeria.component.html',
+  styleUrls: ['./galeria.component.css']
+})
+export class GaleriaComponent {
+
+}
